c2827819112132b653472042479b71a3e81f8f72
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-07-13 21:21:30 +0900

    21:21

--- script.js
+++ script.js
@@ -3,27 +3,3 @@ function color(fontColor, bgColor, label) {
   $('#toggle').val(label);
   $('a').css('color', fontColor);
 }
-/*function nightColor() {
-  document.querySelector('body').style.backgroundColor='rgb(62,65,78)';
-  document.querySelector('body').style.color='white';
-  document.querySelector('a').style.color='white';
-  document.querySelector('#toggle').value='day';
-  var alist = document.querySelectorAll('a');
-  var i = 0;
-  while (i < alist.length) {
-    alist[i].style.color='white';
-    i++
-  }
-}
-function dayColor() {
-  document.querySelector('body').style.backgroundColor='white';
-  document.querySelector('body').style.color='black';
-  document.querySelector('a').style.color='black';
-  document.querySelector('#toggle').value='night';
-  var alist = document.querySelectorAll('a');
-  var i = 0;
-  while (i < alist.length) {
-    alist[i].style.color='black';
-    i++
-  }
-}*/
