aedd54da5081ebea8851639f5b43a54956acbf81
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-07-14 10:54:48 +0900

    10:54

---
+++ node.js/day3.js
@@ -0,0 +1,18 @@
+var a = 1;
+var b = 'one';
+alert(1+1);
+if (prompt('password') === '111111') {
+  alert('주인님 안녕하세요!');
+} else {
+  alert('뉘슈?');
+}
+var a = ['a', 'b', 'c'];
+var i = 0;
+while(i < a.length) {
+  document.write('<li>'+a[i]+'</li>');
+  i++;
+}
+function sum(l, r) {
+  alert(l+r);
+}
+sum(1, 5);
